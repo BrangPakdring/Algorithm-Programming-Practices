2f0e03a4439f5da567817ef47b98ca04e36b0fd6
BrangPakdring <Brang.Pakdring@gmail.com>
2018-09-22 11:37:37 +0800

    cuts comments

--- Templates/Graph Theory/Cut Vertices, Bridges (Tarjan).h
+++ Templates/Graph Theory/Cut Vertices, Bridges (Tarjan).h
@@ -2,19 +2,26 @@
 using namespace std;
 using ll = long long;
 
+/*
+	reference: kuangbin's template
+
+	problem link:
+
+*/
+
 const ll maxn = 105;
 const ll maxm = 100005;
 struct Edge
 {
 	ll from, to, next;
-	bool cut;
+	bool cut; // is cut edge
 }edge[maxm];
 ll head[maxn], tot; // tot total edges added
 ll low[maxn], dfn[maxn], stk[maxn];
 ll idx, top;
 bool inStack[maxn];
-bool cut[maxn];
-ll addBlock[maxn];
+bool cut[maxn]; // is cut vertex
+ll addBlock[maxn]; // addBlock[i] number of connected components increased after deleting vertex i
 ll bridge;
  
 void AddEdge(ll u, ll v)
@@ -33,7 +40,7 @@ void Tarjan(ll u, ll pre)
 	stk[top++] = u;
 	inStack[u] = true;
 	ll son = 0;
-	ll preCnt = 0;
+	ll preCnt = 0; // deal with multiple edges
 	for (ll i = head[u]; i != -1; i = edge[i].next)
 	{
 		v = edge[i].to;
@@ -47,12 +54,14 @@ void Tarjan(ll u, ll pre)
 			++son;
 			Tarjan(v, u);
 			if (low[u] > low[v])low[u] = low[v];
+			// cut edge
 			if (low[v] > dfn[u])
 			{
 				++bridge;
 				edge[i].cut = true;
 				edge[i ^ 1].cut = true;
 			}
+			// cut vertex: u is not a root and u -> v st dfn[u] <= low[v]
 			if (u != pre && low[v] >= dfn[u])
 			{
 				cut[u] = true;
@@ -64,6 +73,7 @@ void Tarjan(ll u, ll pre)
 			low[u] = dfn[v];
 		}
 	}
+	// cut vertex: u is the root with more than one branch
 	if (u == pre && son > 1)cut[u] = true;
 	if (u == pre)addBlock[u] = son - 1;
 	inStack[u] = false;
